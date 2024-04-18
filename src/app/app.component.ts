06a7089924caf4065fa618ea39a026902b490b61
Ruudn7 <slawomirwojciga@MacBook-Pro-Sawomir.local>
2024-04-18 21:18:33 +0200

    initial commit and add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ang17-material';
+
+  openNavbar(navbar: MatSidenav): void {
+    navbar.toggle()
+  }
 }
